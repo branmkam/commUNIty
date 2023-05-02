b3cb78fdedba42ba914bdd1ebc3dac4e0a6414e5
Brandon Kaminski <54331071+branmkam@users.noreply.github.com>
2023-05-02 17:14:42 -0400

    added deals page that displays deals sorted.

--- App.js
+++ App.js
@@ -82,6 +82,11 @@ export default function App() {
   )
 } 
 
+export function parseISOString(s) {
+  var b = s.split(/\D+/);
+  return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+}
+
 
 // //test data
 // const data = [
